e140af754a0c4b488cbed7113533718f72c2a547
Aylur <k.demeter@protonmail.com>
2024-10-17 21:04:08 +0200

    Trying site with quarto instead

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A basic Quarto Falke";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [kntir R quarto rPackages.pagedown rPackages.ggplot2 pandoc ];
+       };
+    });
+}
+
